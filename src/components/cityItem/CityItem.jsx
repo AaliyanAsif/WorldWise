08e6d30fb6425a6a8df0c53a569fec024a8a1b33
Aaliyan Asif <93864353+AaliyanAsif@users.noreply.github.com>
2024-03-04 22:35:02 +0500

    delete city fucn

--- src/components/cityItem/CityItem.jsx
+++ src/components/cityItem/CityItem.jsx
@@ -12,9 +12,13 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 export default function CityItem({ city }) {
-  const { currentCity } = useCities();
+  const { currentCity, deleteCity } = useCities();
   const { cityName, emoji, date, id, position } = city;
-  console.log("city id = ", id);
+
+  function handleClick(e) {
+    e.preventDefault();
+    deleteCity(id);
+  }
   return (
     <li>
       <Link
@@ -26,7 +30,9 @@ export default function CityItem({ city }) {
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>{formatDate(date)}</time>
-        <button className={styles.deleteBtn}>&times;</button>
+        <button onClick={handleClick} className={styles.deleteBtn}>
+          &times;
+        </button>
       </Link>
     </li>
   );
